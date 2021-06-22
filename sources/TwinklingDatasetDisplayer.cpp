f5195a50b9f1e9ad5720394d00ff70bf4fa20744
Nicholas Kulberg <kulberg@yandex.ru>
2021-06-22 13:52:36 +0300

    Чистка устаревшего кода

--- sources/TwinklingDatasetDisplayer.cpp
+++ sources/TwinklingDatasetDisplayer.cpp
@@ -4,68 +4,7 @@
 #include <RFDataImport/S500_CFMFrameSet.h>
 #include <RFDataImport/S500_CFMRawDataDisplay.h>
 
-//#include <DopplerBasics/CFM/WallFilters/WallFiltersInteractive.h>
-
-// тесты универсальных методов доплерографии
-//#include "CFMModesDetailedTest.h"
-
-// локальные тесты тонкостей алгоритмов, довольно беспорядочные
-//#include "SetOfPulses.h"
-//#include "DummyHypothesis.h"
-//#include "SimulateSignal.h"
-
 XRAD_USING;
-
-void	ChooseCFAnalysisTask(S500_CFMFrameSet &frames)
-{
-	size_t	frame_no = frames.n_frames / 2;
-	enum 
-	{ 
-		cfm_modes_detailed_test, 
-		find_blur_1d, 
-//		find_blur_2d, 
-		set_of_pulses, 
-		exit, 
-		n_options 
-	} option;
-	try
-	{
-		while(true)
-		{
-			option = GetButtonDecision("Choose CF analysis task",
-			{
-	//			MakeButton("CFM Modes detailed test",		cfm_modes_detailed_test),
-	//			MakeButton("Find Blur 1D", 	find_blur_1d),
-	//			MakeButton("Find Blur 2D", 	find_blur_2d),
-			//	MakeButton("Set of pulses", set_of_pulses),
-				MakeButton("End with this file",			exit)
-			});
-
-			switch(option)
-			{
-	//			case cfm_modes_detailed_test:
-	//				CFMModesDetailedTest(frames);
-	//				break;
-	// 			case find_blur_1d:
-	// 				CFMMode_find_blur_1D(frames);
-	// 				break;
-	// 			case find_blur_2d:
-	// 				CFMMode_find_blur_2D(frames);
-	// 				break;
-				//case set_of_pulses:
-					//frame_no = GetUnsigned("CFM-frame to display", MakeGUIValue(frames.n_frames / 2, saved_default_value), 0, frames.n_frames - 1);
-					//DisplayASetOfPulses(frames, frame_no);
-				//	break;
-				case exit:
-					throw canceled_operation("Exit button");
-					break;
-			}
-		}
-	}
-	catch(canceled_operation){}
-	catch(quit_application){ throw; }
-	catch(...) { Error(GetExceptionString()); }
-}
 
 
 
@@ -126,41 +65,6 @@ S500_CFMFrameSet GetS500Frames()
 
 
 
-/*
-
-void	WallFilterFrequencyResponse()
-{
-	size_t N = GetUnsigned("Burst Size", SavedGUIValue(16), 4, 1024);
-	ComplexFunctionF32 pulse(N, complexF32(0));
-
-	while(true)
-	{
-		pulse[pulse.size() / 2] = complexF32(1);
-		ComplexFunctionF32	filtered(pulse);
-
-		WallFilterPtr	wall_filter = GetWallFilterInteractive(pulse.size());
-		if(wall_filter->filter_name()==L"WallFilterNone") break;
-
-		wall_filter->Apply(filtered);
-
-		while(true)
-		{
-			auto	fn = GetButtonDecision(
-				L"Display option",
-				{
-					MakeButton(L"Original pulse", make_fn([&pulse](){DisplayMathFunction(pulse, 0, 1, L"Pulse");})),
-					MakeButton(L"WallFilter result",make_fn([&filtered, &wall_filter](){DisplayMathFunction(filtered, 0, 1, L"Filter applied: "+ wall_filter->filter_name());})),
-					MakeButton(L"Exit", function<void()>())
-				}
-			);
-
-			if(fn) fn();
-			else break;
-		}
-	}
-}
-
-*/
 
 int xrad::xrad_main(int, char** const)
 {
@@ -171,8 +75,6 @@ int xrad::xrad_main(int, char** const)
 		size_t	answer(0);
 		while (true)
 		{
-			//answer = Decide("Choose source", { "Process raw S500 data", "Process simulated signal", "Wall filter frequency response", "Test dummy hypothesis", "Exit" });
-			//answer = Decide("Choose source", { "Process raw S500 data", "Process simulated signal", "Test dummy hypothesis", "Exit" });
 			answer = Decide("Choose source", { "Process raw S500 data", "Exit" });
 			switch (answer)
 			{
